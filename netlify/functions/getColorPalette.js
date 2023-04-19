a28d6a3756fb32fe8bd62fa78bad4957d341c891
Elron <elron100duck@gmail.com>
2023-04-19 21:55:50 +0300

    retry

--- netlify/functions/getColorPalette.js
+++ netlify/functions/getColorPalette.js
@@ -1,52 +1,52 @@
-const chromium = require('chrome-aws-lambda');
+const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
 
-
-const ColorThief = require('colorthief');
+const ColorThief = require("colorthief");
 
 exports.handler = async (event) => {
-	const url = event.queryStringParameters.url;
+  const url = event.queryStringParameters.url;
 
-	if (!url) {
-		return {
-			statusCode: 400,
-			body: 'Missing URL parameter'
-		};
-	}
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: "Missing URL parameter",
+    };
+  }
 
-	try {
-		
-		const browser = await chromium.puppeteer.launch({
-			executablePath: await chromium.executablePath,
-			args: chromium.args,
-			defaultViewport: chromium.defaultViewport,
-			headless: chromium.headless,
-		});
-	
-		const page = await browser.newPage();
-		await page.goto(url);
-		const screenshotBuffer = await page.screenshot({ fullPage: true });
-		await browser.close();
+  try {
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath:
+        process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
+      headless: true,
+    });
 
-		const colorThief = new ColorThief();
-		const colors = await colorThief.getPalette(screenshotBuffer, 5);
+    const page = await browser.newPage();
+    await page.goto(url);
+    const screenshotBuffer = await page.screenshot({ fullPage: true });
+    await browser.close();
 
-		const hexColors = colors.map(
-			(color) => `#${color.map((c) => c.toString(16).padStart(2, '0')).join('')}`
-		);
+    const colorThief = new ColorThief();
+    const colors = await colorThief.getPalette(screenshotBuffer, 5);
 
-		return {
-			statusCode: 200,
-			body: JSON.stringify(hexColors),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		};
-	} catch (error) {
-		console.error(error);
+    const hexColors = colors.map(
+      (color) =>
+        `#${color.map((c) => c.toString(16).padStart(2, "0")).join("")}`
+    );
 
-		return {
-			statusCode: 500,
-			body: 'Internal Server Error'
-		};
-	}
+    return {
+      statusCode: 200,
+      body: JSON.stringify(hexColors),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      statusCode: 500,
+      body: "Internal Server Error",
+    };
+  }
 };
