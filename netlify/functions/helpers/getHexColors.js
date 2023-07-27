1c5340dfd8778e1b5ee233b812b7125d17de2a13
Elron <elron100duck@gmail.com>
2023-07-27 06:31:47 +0300

    getHexColors___url_css_files_popular_hex

--- netlify/functions/helpers/getHexColors.js
+++ netlify/functions/helpers/getHexColors.js
@@ -1,6 +1,6 @@
 // import { getHexColors___puppeteer_url_screenshot_colorthief } from "./modules/puppeteer-url-screenshot-colortheif.js";
-// import { getHexColors___url_css_files_popular_hex } from "./modules/url-css-files-popular-hex.js";
-import { getHexColors___url_thumbio_image_getImageColors } from "./modules/url-thumio-image-getImageColors.js";
+import { getHexColors___url_css_files_popular_hex } from "./modules/url-css-files-popular-hex.js";
+// import { getHexColors___url_thumbio_image_getImageColors } from "./modules/url-thumio-image-getImageColors.js"; // works in netlify
 
 // Function to run three promises and return the value of the first resolved promise
 function runPromises(promises) {
@@ -25,9 +25,9 @@ function runPromises(promises) {
 
 export async function getHexColors(url) {
   const hexColorsTries = [
-    // getHexColors___url_css_files_popular_hex(url),
+    getHexColors___url_css_files_popular_hex(url),
     // getHexColors___puppeteer_url_screenshot_colorthief(url),
-    getHexColors___url_thumbio_image_getImageColors(url),
+    // getHexColors___url_thumbio_image_getImageColors(url),
   ];
   const hexColors = await runPromises(hexColorsTries);
 
