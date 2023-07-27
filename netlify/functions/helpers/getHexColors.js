e8ec4268bad49ac17bd4a23efd1ddf94c0ac8d3c
Elron <elron100duck@gmail.com>
2023-07-27 06:12:40 +0300

    runPromises

--- netlify/functions/helpers/getHexColors.js
+++ netlify/functions/helpers/getHexColors.js
@@ -2,26 +2,26 @@
 // import { getHexColors___url_css_files_popular_hex } from "./modules/url-css-files-popular-hex.js";
 // import { getHexColors___url_thumbio_image_getImageColors } from "./modules/url-thumio-image-getImageColors.js";
 
-// // Function to run three promises and return the value of the first resolved promise
-// function runPromises(promises) {
-//   // Helper function to handle promises and resolve with the first resolved value
-//   function resolveFirst(promises) {
-//     return new Promise((resolve) => {
-//       promises.forEach((promise) => {
-//         promise
-//           .then((value) => {
-//             resolve(value);
-//           })
-//           .catch(() => {
-//             // Ignore rejected promises
-//           });
-//       });
-//     });
-//   }
+// Function to run three promises and return the value of the first resolved promise
+function runPromises(promises) {
+  // Helper function to handle promises and resolve with the first resolved value
+  function resolveFirst(promises) {
+    return new Promise((resolve) => {
+      promises.forEach((promise) => {
+        promise
+          .then((value) => {
+            resolve(value);
+          })
+          .catch(() => {
+            // Ignore rejected promises
+          });
+      });
+    });
+  }
 
-//   // Call the helper function and return the first resolved value
-//   return resolveFirst(promises);
-// }
+  // Call the helper function and return the first resolved value
+  return resolveFirst(promises);
+}
 
 export async function getHexColors(url) {
   // const hexColorsTries = [
